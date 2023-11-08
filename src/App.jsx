fff7da9ca921d82c0269f5c08e84ede0d4293c91
Kristjan Lääne <xonproject@gmail.com>
2023-11-08 16:59:13 +0200

    setting up categories

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
-import './App.css';
+const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+    },
+    {
+      id: 4,
+      title: 'Womens',
+    },
+    {
+      id: 5,
+      title: 'Mens',
+    },
+  ];
 
-function App() {
-  return <div>Hi</div>;
-}
+  return (
+    <div className='categories-container'>
+      {categories.map(({ title, id }) => (
+        <div key={id} className='category-container'>
+          <div className='backgound-image' />
+          <div className='category-body-container'>
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default App;
